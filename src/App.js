8e6e72d24837c40b775e838a3b08748ec73cc21b
walid-01 <71653653+walid-01@users.noreply.github.com>
2022-10-04 21:27:05 +0100

    finished footer and /collections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Collections from "./components/Collections";
+import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,7 +12,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/collections" exact element={<Collections />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
